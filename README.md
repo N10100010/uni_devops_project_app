5b58f0b2efc4d9fc8b63d6f250aee908fcea3db7
Nick <nr034@hdm-stuttgart.de>
2022-06-06 09:16:24 +0200

    inint

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# uni_devops_project_app
+# uni-devops-project-app
+
+This Repository is a sub-repository of the uni-devops-project. Contrary to the surrounding repository, this one actually holds the application code.
+
+It is supposed to be pulled together with the infrastructure code, since this is not a stand-alone application. It is thought to be run in a container.
---
+++ views.py
@@ -0,0 +1,42 @@
+from app import app
+from flask import Flask, request, jsonify, render_template
+
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+
+    name = request.args.get("name", None)
+
+    response = {}
+
+    if not name:
+        response["ERROR"] = "No name found. Please send a name."
+
+    elif str(name).isdigit():
+        response["ERROR"] = "The name can't be numeric. Please send a string."
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+
+    return jsonify(response)
+
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome API!",
+
+            "METHOD": "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "No name found. Please send a name."
+        })
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
